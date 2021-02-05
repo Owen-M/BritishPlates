cce68187fad70910bfa3d36ca8476288b3ebfafe
NotSomething0 <programming.unconsociated@aleeas.com>
2021-02-05 12:48:51 -0600

    Use GetGamePool instead of a really unoptimized pieace of code.

--- client.lua
+++ client.lua
@@ -4,7 +4,7 @@ local plateLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M"
 
 Citizen.CreateThread(function()
     while true do
-        for vehicle in EnumerateVehicles() do
+        for vehicle in GetGamePool('CVehicle') do
             local currentPlate = GetVehicleNumberPlateText(vehicle)
 			
             if currentPlate ~= nil then
@@ -31,41 +31,3 @@ Citizen.CreateThread(function()
         Citizen.Wait(500)
     end
 end)
-
-local entityEnumerator = {
-    __gc = function(enum)
-        if enum.destructor and enum.handle then
-            enum.destructor(enum.handle)
-        end
-        enum.destructor = nil
-        enum.handle = nil
-    end
-}
-  
-local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
-    return coroutine.wrap(function()
-        local iter, id = initFunc()
-
-        if not id or id == 0 then
-            disposeFunc(iter)
-            return
-        end
-        
-        local enum = {handle = iter, destructor = disposeFunc}
-        setmetatable(enum, entityEnumerator)
-        
-        local next = true
-
-        repeat
-            coroutine.yield(id)
-            next, id = moveFunc(iter)
-        until not next
-        
-        enum.destructor, enum.handle = nil, nil
-        disposeFunc(iter)
-    end)
-end
-  
-function EnumerateVehicles()
-    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
-end
